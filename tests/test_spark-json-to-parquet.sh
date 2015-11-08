231f81fd3ebcf7bf51c0f808f741f1596482c48e
Hari Sekhon <harisekhon@gmail.com>
2015-11-08 14:44:03 +0000

    updated github repo references

--- tests/test_spark-json-to-parquet.sh
+++ tests/test_spark-json-to-parquet.sh
@@ -4,7 +4,7 @@
 #  Author: Hari Sekhon
 #  Date: 2015-11-05 23:29:15 +0000 (Thu, 05 Nov 2015)
 #
-#  https://github.com/harisekhon
+#  https://github.com/harisekhon/pytools
 #
 #  License: see accompanying Hari Sekhon LICENSE file
 #
