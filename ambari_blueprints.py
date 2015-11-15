f06a602bee708d511bec8da480dfaea353782191
Hari Sekhon <harisekhon@gmail.com>
2015-11-15 15:38:14 +0000

    added sorting of hostgroup components for consistent revision controlling of ambari blueprints

--- ambari_blueprints.py
+++ ambari_blueprints.py
@@ -38,7 +38,7 @@ Tested on Ambari 2.1.0 and Hortonworks HDP 2.2 / 2.3 clusters
 from __future__ import print_function
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 
 import base64
 from httplib import BadStatusLine
@@ -218,6 +218,7 @@ class AmbariBlueprintTool():
                 del jsonData['configurations']
                 for hostgroup in jsonData['host_groups']:
                     del hostgroup['configurations']
+                    hostgroup['components'] = list_sort_dicts_by_value(hostgroup['components'], 'name')
             except KeyError, e:
                 pass
         return jsonpp(jsonData)
