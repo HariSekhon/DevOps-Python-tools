c6e617d9dccdd6fb2738017296a579aab7360cc2
Hari Sekhon <harisekhon@gmail.com>
2015-11-17 12:46:31 +0000

    added tests/welcome.sh

---
+++ tests/welcome.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-11-05 23:29:15 +0000 (Thu, 05 Nov 2015)
+#
+#  https://github.com/harisekhon/pytools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -eu
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$srcdir/..";
+
+. tests/travis.sh
+
+cd "$srcdir"
+
+./welcome.py
