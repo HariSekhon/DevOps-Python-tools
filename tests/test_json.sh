f7902c975fffbff703397ca52d00cd5ec968ef69
Hari Sekhon <harisekhon@gmail.com>
2015-11-20 21:24:42 +0000

    added tests/test_json.sh

---
+++ tests/test_json.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2015-11-05 23:29:15 +0000 (Thu, 05 Nov 2015)
+#
+#  https://github.com/harisekhon/pytools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  http://www.linkedin.com/in/harisekhon
+#
+
+set -eu
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "
+# ================== #
+# Testing JSON Files 
+# ================== #
+"
+
+cd "$srcdir/..";
+
+. tests/travis.sh
+
+find . -iname '*.json' |
+grep -v '/spark-.*-bin-hadoop.*/' |
+# ignore multi-line json data file for spark testing
+grep -v 'tests/test.json' |
+while read jsonFile; do
+    echo "testing json file: $jsonFile"
+    python -mjson.tool < "$jsonFile" > /dev/null
+done
+
+echo
+echo
