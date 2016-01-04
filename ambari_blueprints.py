1c4a45dba96384254c510527afe2ca5b57cef4be
Hari Sekhon <harisekhon@gmail.com>
2016-01-04 17:22:06 +0000

    updated ambari_blueprints.py

--- ambari_blueprints.py
+++ ambari_blueprints.py
@@ -22,9 +22,9 @@ Features:
 
 1. find and fetch all Ambari Blueprints and/or Blueprint existing clusters
 2. fetch a specific given blueprint
-3. blueprint an existing cluster (<== I like this one)
+3. blueprint an existing cluster (<== use this one!)
 4. strips out href that is not valid to re-submit
-5. strips out configuration settings values to make the blueprint more generic if specifying --strip-config
+5. use --strip-config to strip out configuration settings values to make the blueprint more generic
 6. push a given blueprint file to Ambari, resetting a blueprint's name field on the fly to avoid field conflicts between adjacent blueprints
 7. create a new cluster using a previously uploaded blueprint and a hostmapping file
 8. list available blueprints, clusters and hosts
