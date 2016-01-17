b7b97121889dbe254b9c98573349bfc5ce492832
Hari Sekhon <harisekhon@gmail.com>
2016-01-17 13:51:45 +0000

    updated tests/all.sh

--- tests/all.sh
+++ tests/all.sh
@@ -40,4 +40,5 @@ done
 # do help afterwards for Spark to be downloaded, and then help will find and use downloaded spark for SPARK_HOME
 ./help.sh
 
-./travis.sh
+cd "$srcdir/.."
+bash-tools/travis.sh
