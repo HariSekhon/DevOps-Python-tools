0c606c79e2b133a9457b9b087e328485a4b0fc5c
Hari Sekhon <harisekhon@gmail.com>
2016-01-31 00:53:20 +0000

    updated center.py

--- center.py
+++ center.py
@@ -31,8 +31,9 @@ import sys
 libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'pylib'))
 sys.path.append(libdir)
 try:
-    from harisekhon.utils import isChars  # pylint: disable=wrong-import-position
-    from harisekhon import CLI            # pylint: disable=wrong-import-position
+    # pylint: disable=wrong-import-position
+    from harisekhon.utils import isChars
+    from harisekhon import CLI
 except ImportError as _:
     print('module import failed: %s' % _, file=sys.stderr)
     print("Did you remember to build the project by running 'make'?", file=sys.stderr)
