f55e140f6d6d335b619cb6c3726527f2cc503d94
Hari Sekhon <harisekhon@gmail.com>
2016-05-02 22:54:44 +0100

    updated validate_multimedia.py

--- validate_multimedia.py
+++ validate_multimedia.py
@@ -31,6 +31,9 @@ Directories are recursed, checking any files ending in one of the following exte
 .mkv
 .wmv
 
+or if given a --regex will check any matching filenames in the directories given. Files explicitly given on the
+command line are always checked.
+
 """
 
 from __future__ import absolute_import
@@ -47,7 +50,7 @@ libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'pylib'))
 sys.path.append(libdir)
 try:
     # pylint: disable=wrong-import-position
-    from harisekhon.utils import die, ERRORS, log, log_option, uniq_list_ordered, which
+    from harisekhon.utils import die, log, log_option, uniq_list_ordered, which, validate_regex
     from harisekhon import CLI
 except ImportError as _:
     print('module import failed: %s' % _, file=sys.stderr)
@@ -56,7 +59,7 @@ except ImportError as _:
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.7.5'
+__version__ = '0.7.6'
 
 class MediaValidatorTool(CLI):
 
@@ -65,30 +68,53 @@ class MediaValidatorTool(CLI):
         super(MediaValidatorTool, self).__init__()
         # Python 3.x
         # super().__init__()
-        self.re_media_suffix = re.compile(r'.*\.(?:mp[34]|mpe?g|m4a|avi|flv|mkv|wmv)$', re.I)
         self.failed = False
-        self.validate_cmd = "ffmpeg -v error -f null - -i"
+        self.re_media_suffix = re.compile(r'.*\.(?:mp[34]|mpe?g|m4a|avi|flv|mkv|wmv)$', re.I)
+        self.skip_errors = None
+        self.quick = None
+        self.regex = None
         self.timeout_default = 0
+        self.validate_cmd = "ffmpeg -v error -f null - -i"
 
     def add_options(self):
+        self.add_opt('-r', '--regex', default=None,
+                     help='Regex of files to check when recursing a directory ' +
+                     '(case insensitive, replaces extension check)')
         self.add_opt('-q', '--quick', action='store_true', default=False,
                      help="Quick mode (uses 'ffprobe' instead of 'ffmpeg')")
         self.add_opt('-c', '--continue', action='store_true', default=False,
-                     help='Continue after finding a broken multimedia file if recursing a directory tree')
+                     help='Continue checking remaining files after finding a broken multimedia file')
 
-    def run(self):
-        log_option('quick', self.get_opt('quick'))
-        log_option('continue-on-error', self.get_opt('continue'))
-        if self.get_opt('quick'):
-            self.validate_cmd = "ffprobe"
+    def process_args(self):
+        self.skip_errors = self.get_opt('continue')
+        self.quick = self.get_opt('quick')
+        self.regex = self.get_opt('regex')
+        args = uniq_list_ordered(self.args)
+        if not args:
+            self.usage('no files/dirs specified')
+        log_option('files/dirs', args)
+        log_option('regex', self.regex)
+        log_option('quick', self.quick)
+        log_option('continue-on-error', self.skip_errors)
+        if self.regex:
+            validate_regex(self.regex)
+            self.regex = re.compile(self.regex, re.I)
+        if self.quick:
+            self.validate_cmd = 'ffprobe'
         if not which(self.validate_cmd.split()[0]):
             die('ffmpeg / ffprobe not found in $PATH')
-        args = uniq_list_ordered(self.args)
-        #log_option('files/dirs', args)
+        return args
+
+    def run(self):
+        args = self.process_args()
         for arg in args:
             if not os.path.exists(arg):
-                print("'%s' not found" % arg)
-                sys.exit(ERRORS['WARNING'])
+                _ = "'%s' not found" % arg
+                if self.skip_errors:
+                    print(_)
+                    self.failed = True
+                else:
+                    die(_)
             if os.path.isfile(arg):
                 log_option('file', arg)
             elif os.path.isdir(arg):
@@ -98,18 +124,21 @@ class MediaValidatorTool(CLI):
         for arg in args:
             self.check_path(arg)
         if self.failed:
-            sys.exit(ERRORS['CRITICAL'])
-        if self.failed:
-            sys.exit(1)
+            sys.exit(2)
 
     def check_path(self, path):
         if os.path.isfile(path):
+            # files given explicitly are checked regardless
+            # if self.regex and self.regex.search(path):
             self.check_media_file(path)
         elif os.path.isdir(path):
             for item in os.listdir(path):
                 subpath = os.path.join(path, item)
                 if os.path.isdir(subpath):
                     self.check_path(subpath)
+                elif self.regex:
+                    if self.regex.search(item):
+                        self.check_media_file(subpath)
                 elif self.re_media_suffix.match(item):
                     self.check_media_file(subpath)
         else:
@@ -139,7 +168,7 @@ class MediaValidatorTool(CLI):
         except CalledProcessError as _:
             if self.verbose > 2:
                 print(_.output)
-            if self.get_opt('continue'):
+            if self.skip_errors:
                 print(invalid_media_msg)
                 self.failed = True
                 return False
