54fa0e61c0935a01b901a2025ba75677f36da61c
Hari Sekhon <harisekhon@gmail.com>
2016-06-01 17:03:49 +0100

    updated test_hbase.sh

--- tests/test_hbase.sh
+++ tests/test_hbase.sh
@@ -38,7 +38,12 @@ export HBASE_HOST
 export HBASE_STARGATE_PORT=8080
 #export HBASE_THRIFT_PORT=9090
 
-export DOCKER_IMAGE="harisekhon/hbase-dev"
+#export HBASE_VERSIONS="0.96 0.98 1.0 1.1 1.2"
+# don't work
+#export HBASE_VERSIONS="0.98 0.96"
+export HBASE_VERSIONS="1.0 1.1 1.2"
+
+export DOCKER_IMAGE="harisekhon/hbase"
 export DOCKER_CONTAINER="hbase-test"
 
 if ! is_docker_available; then
@@ -48,27 +53,35 @@ fi
 
 startupwait=50
 
-hr
-echo "Setting up HBase test container"
-hr
-launch_container "$DOCKER_IMAGE" "$DOCKER_CONTAINER" 2181 8080 8085 9090 9095 16000 16010 16201 16301
+test_hbase(){
+    local version="$1"
+    hr
+    echo "Setting up HBase $version test container"
+    hr
+    launch_container "$DOCKER_IMAGE:$version" "$DOCKER_CONTAINER" 2181 8080 8085 9090 9095 16000 16010 16201 16301
 
-echo "setting up test tables"
-uniq_val=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | head -c32 || :)
-docker exec -i "$DOCKER_CONTAINER" /bin/bash <<EOF
-export JAVA_HOME=/usr
-/hbase/bin/hbase shell <<EOF2
-create 't1', 'cf1', { REGION_REPLICATION => 1 }
-create 't2', 'cf2', { REGION_REPLICATION => 1 }
-disable 't2'
-put 't1', 'r1', 'cf1:q1', '$uniq_val'
-put 't1', 'r2', 'cf1:q2', 'test'
-list
+    echo "setting up test tables"
+    uniq_val=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | head -c32 || :)
+    docker exec -i "$DOCKER_CONTAINER" /bin/bash <<-EOF
+        export JAVA_HOME=/usr
+        /hbase/bin/hbase shell <<-EOF2
+        create 't1', 'cf1', { 'REGION_REPLICATION' => 1 }
+        create 't2', 'cf2', { 'REGION_REPLICATION' => 1 }
+        disable 't2'
+        put 't1', 'r1', 'cf1:q1', '$uniq_val'
+        put 't1', 'r2', 'cf1:q2', 'test'
+        list
 EOF2
 EOF
 
-hr
-./hbase_compact_tables.py -H $HBASE_HOST
-hr
+    hr
+    ./hbase_compact_tables.py -H $HBASE_HOST
+    hr
 
-delete_container
+    delete_container
+    echo
+}
+
+for version in $HBASE_VERSIONS; do
+    test_hbase $version
+done
