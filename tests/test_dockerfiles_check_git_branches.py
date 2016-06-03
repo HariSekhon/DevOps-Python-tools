770078e20ebf900b0d96217127d4906b5ebd3422
Hari Sekhon <harisekhon@gmail.com>
2016-06-03 16:04:54 +0100

    added tests/test_dockerfiles_check_git_branches.py

---
+++ tests/test_dockerfiles_check_git_branches.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2016-01-16 16:35:51 +0000 (Sat, 16 Jan 2016)
+#
+#  https://github.com/harisekhon/pytools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help improve or steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "$srcdir/.."
+
+. "tests/utils.sh"
+
+. "bash-tools/utils.sh"
+
+section "Testing DockerfileS Check Git branches"
+
+check './dockerfiles_check_git_branches.py Dockerfiles' "Dockerfiles Check Git branches of submodule Dockerfiles/"
+
+echo
+echo
