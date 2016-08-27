e5e6d6fbb63172c5f14206320a6ea4078ad29400
Hari Sekhon <harisekhon@gmail.com>
2016-08-27 11:23:19 +0100

    updated test_find_duplicate_files.sh

--- tests/test_find_duplicate_files.sh
+++ tests/test_find_duplicate_files.sh
@@ -24,62 +24,108 @@ cd "$srcdir/.."
 
 section "find_duplicate_files.py"
 
-testdir="$(cd tests/data/ && mktemp -d -t tmp_find_duplicate_files.XXXXXX)"
+testdir1="$(cd tests/data/ && mktemp -d -t tmp_find_duplicate_files.XXXXXX)"
 testdir2="$(cd tests/data/ && mktemp -d -t tmp_find_duplicate_files2.XXXXXX)"
 
-mkdir -v -p "$testdir"
-
-echo test > "$testdir/test1.txt"
-echo nonmatching > "$testdir/nonmatching.txt"
+echo test > "$testdir1/test1.txt"
+echo nonmatching > "$testdir1/nonmatching.txt"
 
 echo "checking no dups:"
 echo
-./find_duplicate_files.py "$testdir"
+./find_duplicate_files.py "$testdir1"
+echo
+echo "checking no dups even when giving duplicate directory args:"
+./find_duplicate_files.py "$testdir1" "$testdir1"
+echo
+echo "checking no dups in quiet mode:"
+./find_duplicate_files.py --quiet "$testdir1" "$testdir1"
 echo
 
 hr
 
+for testdir in "$testdir1" "$testdir2"; do
+    if [ "$testdir" = "$testdir1" ]; then
+        msg2="in the same directory tree"
+    else
+        msg2="in different directory trees"
+    fi
 
-echo "checking for dups by name in same directory tree:"
-mkdir "$testdir/2"
-echo different > "$testdir/2/test1.txt"
-echo
-set +e
-./find_duplicate_files.py "$testdir" "$testdir2"
-check_exit_code 1
-echo
-rm "$testdir/2/test1.txt"
+    echo "checking for dups by name $msg2:"
+    mkdir "$testdir/2"
+    echo different > "$testdir/2/test1.txt"
+    echo
+    set +e
+    ./find_duplicate_files.py "$testdir" "$testdir1"
+    check_exit_code 1
+    set -e
+    echo
+    rm "$testdir/2/test1.txt"
 
-hr
+    hr
 
-echo "checking for dups by checksum in same directory tree:"
-echo test > "$testdir/test2.txt"
-echo
-./find_duplicate_files.py "$testdir" "$testdir2"
-check_exit_code 1
-echo
-rm "$testdir/test2.txt"
+    echo "checking for dups by size $msg2:"
+    echo abcd > "$testdir/test2.txt"
+    echo
+    set +e
+    ./find_duplicate_files.py --size "$testdir" "$testdir1"
+    check_exit_code 1
+    set -e
+    echo
+    echo "checking dups by hash doesn't match on differing contents $msg2:"
+    ./find_duplicate_files.py --checksum "$testdir" "$testdir1"
+    echo "and with no options specified $msg2:"
+    ./find_duplicate_files.py "$testdir" "$testdir1"
+    echo
+    rm "$testdir/test2.txt"
 
-hr
+    hr
 
-echo "checking for dups by name across directory trees:"
-echo different > "$testdir2/test1.txt"
-echo
-./find_duplicate_files.py "$testdir" "$testdir2"
-check_exit_code 1
-echo
-rm "$testdir2/test1.txt"
+    echo "checking for dups by checksum $msg2:"
+    echo test > "$testdir/test3.txt"
+    echo
+    set +e
+    ./find_duplicate_files.py --checksum "$testdir" "$testdir1"
+    check_exit_code 1
+    set -e
+    echo
+    rm "$testdir/test3.txt"
 
-hr
+    hr
 
-echo "checking for dups by checksum across directory trees:"
-echo test > "$testdir2/test4.txt"
-echo
-./find_duplicate_files.py "$testdir" "$testdir2"
-check_exit_code 1
-echo
+    echo "checking for dups by regex capture $msg2:"
+    echo test2 > "$testdir/test2.txt"
+    echo
+    echo "first check no other method matches:"
+    ./find_duplicate_files.py "$testdir" "$testdir1"
+    echo
 
-rm -fr "$testdir" "$testdir2"
+    hr
+
+    echo "now check the file basename matches on 'est'":
+    set +e
+    ./find_duplicate_files.py --regex 'est' "$testdir" "$testdir1" --quiet
+    check_exit_code 1
+    echo
+
+    hr
+
+    echo "now check the file basename matches with specified capture subset '(est)\d'":
+    ./find_duplicate_files.py --regex '(est)\d' "$testdir" "$testdir1"
+    check_exit_code 1
+    set -e
+    echo
+
+    hr
+
+    echo "now check the file basename doesn't match when the capture includes differing numbers 'est\d'":
+    ./find_duplicate_files.py --regex 'est\d' "$testdir" "$testdir1"
+    echo
+    rm "$testdir/test2.txt"
+
+    hr
+done
+
+rm -fr "$testdir1" "$testdir2"
 
 echo
 echo
