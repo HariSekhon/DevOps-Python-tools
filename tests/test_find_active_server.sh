559debd295e59cd521dd1d880e099c14e6c4eebf
Hari Sekhon <harisekhon@gmail.com>
2016-10-04 12:56:43 +0100

    updated test_find_active_server.sh

--- tests/test_find_active_server.sh
+++ tests/test_find_active_server.sh
@@ -42,33 +42,41 @@ unset HOST
 # 0.0.0.1 ping => connect: Invalid argument
 # 4.4.4.4 ping => no response
 
+opts="-v"
+
+# Travis CI seems to fail to find things online when they clearly are available, perhaps it's the network delay, increasing
+# per request timeout to try to make this more tolerant
+if is_CI; then
+    opts="$opts --request-timeout 5 --timeout 30"
+fi
+
 echo "testing socket ordering result consistency"
 echo
 
-check_output "yahoo.com" ./find_active_server.py --verbose --num-threads 1 --port 80 yahoo.com google.com
+check_output "yahoo.com" ./find_active_server.py $opts --num-threads 1 --port 80 yahoo.com google.com
 
-check_output "google.com" ./find_active_server.py --verbose --num-threads 1 --port 80 0.0.0.1 4.4.4.4 google.com yahoo.com
+check_output "google.com" ./find_active_server.py $opts --num-threads 1 --port 80 0.0.0.1 4.4.4.4 google.com yahoo.com
 
 hr
 echo "testing socket ordering result consistency with individual port overrides"
 echo
 
-check_output "yahoo.com:80" ./find_active_server.py -v -n1 yahoo.com:80 google.com --port 1
+check_output "yahoo.com:80" ./find_active_server.py $opts -n1 yahoo.com:80 google.com --port 1
 
-check_output "google.com:80" ./find_active_server.py -v -n1 yahoo.com google.com:80 --port 1
+check_output "google.com:80" ./find_active_server.py $opts -n1 yahoo.com google.com:80 --port 1
 
 # ============================================================================ #
 hr
 echo "checking --ping and --port switch conflict fails"
 echo
-./find_active_server.py -v -n1 0.0.0.1 4.4.4.4 google.com --ping --port 1
+./find_active_server.py $opts -n1 0.0.0.1 4.4.4.4 google.com --ping --port 1
 check_exit_code 3
 echo
 
 hr
 echo "checking --ping and --http switch conflict fails"
 echo
-./find_active_server.py -v -n1 0.0.0.1 4.4.4.4 google.com --ping --http
+./find_active_server.py $opts -n1 0.0.0.1 4.4.4.4 google.com --ping --http
 check_exit_code 3
 echo
 
@@ -76,59 +84,59 @@ echo
 hr
 echo "testing ping ordering result consistency"
 echo
-check_output "google.com" ./find_active_server.py -v -n1 0.0.0.1 4.4.4.4 google.com --ping
+check_output "google.com" ./find_active_server.py $opts -n1 0.0.0.1 4.4.4.4 google.com --ping
 
 hr
 echo "testing ping ordering result consistency with individual port overrides"
 echo
 
-check_output "google.com" ./find_active_server.py -v -n1 0.0.0.1 4.4.4.4 google.com:80 --ping
+check_output "google.com" ./find_active_server.py $opts -n1 0.0.0.1 4.4.4.4 google.com:80 --ping
 
 # ============================================================================ #
 hr
 echo "testing http ordering result consistency"
 echo
 
-check_output "yahoo.com" ./find_active_server.py -v -n1 0.0.0.1 4.4.4.4 yahoo.com google.com --http 
+check_output "yahoo.com" ./find_active_server.py $opts -n1 0.0.0.1 4.4.4.4 yahoo.com google.com --http
 
-check_output "google.com" ./find_active_server.py -v -n1 google.com yahoo.com --http 
+check_output "google.com" ./find_active_server.py $opts -n1 google.com yahoo.com --http
 
 hr
 echo "testing https ordering result consistency"
 echo
 
-check_output "yahoo.com" ./find_active_server.py -v -n1 yahoo.com google.com --https 
+check_output "yahoo.com" ./find_active_server.py $opts -n1 yahoo.com google.com --https
 
-check_output "google.com" ./find_active_server.py -v -n1 0.0.0.1 4.4.4.4 google.com yahoo.com --https 
+check_output "google.com" ./find_active_server.py $opts -n1 0.0.0.1 4.4.4.4 google.com yahoo.com --https
 
 echo
 echo "testing https returns no results when using wrong port 25"
 echo
 
 # DEBUG=1 breaks this to return NO_AVAILABLE_SERVER
-#check_output "" ./find_active_server.py -v mail.google.com --https --port 25
+#check_output "" ./find_active_server.py $opts mail.google.com --https --port 25
 
-check_output "NO_AVAILABLE_SERVER" ./find_active_server.py -v mail.google.com --https --port 25
+check_output "NO_AVAILABLE_SERVER" ./find_active_server.py $opts mail.google.com --https --port 25
 
 echo
 echo "testing https with url suffix and regex matching"
 echo
 
-check_output "github.com" ./find_active_server.py -v --https google.com github.com -u /harisekhon --regex 'pytools'
+check_output "github.com" ./find_active_server.py $opts --https google.com github.com -u /harisekhon --regex 'pytools'
 
 # ============================================================================ #
 hr
 echo "testing HTTP regex filtering"
 echo
 
-check_output "yahoo.com" ./find_active_server.py -v google.com yahoo.com --http --regex 'yahoo'
+check_output "yahoo.com" ./find_active_server.py $opts google.com yahoo.com --http --regex 'yahoo'
 
 # ============================================================================ #
 hr
 echo "testing HTTPS regex filtering"
 echo
 
-check_output "yahoo.com" ./find_active_server.py -v google.com yahoo.com --https --regex '(?:yahoo)'
+check_output "yahoo.com" ./find_active_server.py $opts google.com yahoo.com --https --regex '(?:yahoo)'
 
 # ============================================================================ #
 hr
