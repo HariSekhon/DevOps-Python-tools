2c0f7d7d9a0a8708e2bed0db98aa60b7c1221c33
Hari Sekhon <harisekhon@gmail.com>
2017-08-13 11:00:04 +0100

    overhaul of tests, unified sample json data files

--- tests/test_validate_json.sh
+++ tests/test_validate_json.sh
@@ -37,42 +37,31 @@ exclude='/tests/spark-\d+\.\d+.\d+-bin-hadoop\d+.\d+$|broken|error'
 rm -fr "$broken_dir" || :
 mkdir "$broken_dir"
 
+echo "checking all JSON files in local directory"
 ./validate_json.py --exclude "$exclude" .
 echo
 
+# ==================================================
+hr2
+echo "checking multirecord json"
+./validate_json.py "$data_dir/multirecord.json"
+echo
+
+# ==================================================
+hr2
 echo "checking directory recursion (mixed with explicit file given)"
 ./validate_json.py "$data_dir/test.json" .
 echo
 
+# ==================================================
+hr2
 echo "checking json file without an extension"
 cp -iv "$(find "${1:-.}" -iname '*.json' | grep -v -e '/spark-.*-bin-hadoop.*/' -e 'broken' -e 'error' | head -n1)" "$broken_dir/no_extension_testfile"
 ./validate_json.py -t 1 "$broken_dir/no_extension_testfile"
 echo
 
-echo "checking single quoted json"
-./validate_json.py -s "$data_dir/single_quotes.notjson"
-echo
-
-echo "checking multirecord single quoted json multirecord"
-./validate_json.py -s "$data_dir/single_quotes_multirecord.notjson"
-echo
-
-echo "checking multirecord single quoted json with mixed quoting"
-./validate_json.py -s "$data_dir/single_quotes_multirecord_mixed_quotes.notjson"
-echo
-
-echo "checking multirecord single quoted json with embedded double quotes"
-./validate_json.py -s "$data_dir/single_quotes_embedded_double_quotes.notjson"
-echo
-
-echo "checking multirecord single quoted json with embedded double quotes"
-./validate_json.py -s "$data_dir/single_quotes_embedded_double_quotes.notjson"
-echo
-
-echo "checking multirecord json with embedded non-escaped double quotes"
-./validate_json.py -s "$data_dir/single_quotes_embedded_double_quotes_unescaped.notjson"
-echo
-
+# ==================================================
+hr2
 echo "testing stdin"
 ./validate_json.py - < "$data_dir/test.json"
 ./validate_json.py < "$data_dir/test.json"
@@ -82,6 +71,8 @@ echo "testing stdin with multi-record"
 ./validate_json.py -m - < "$data_dir/multirecord.json"
 echo
 
+# ==================================================
+hr2
 echo "checking symlink handling"
 ln -sfv "test.json" "$data_dir/testlink.json"
 ./validate_json.py "$data_dir/testlink.json"
@@ -109,6 +100,8 @@ check_broken(){
     fi
 }
 
+# ==================================================
+hr2
 echo "checking normal json stdin breakage using --multi-record switch"
 set +e
 ./validate_json.py - -m < "$data_dir/test.json"
@@ -118,18 +111,35 @@ if [ $exitcode = 2 ]; then
     echo "successfully detected breakage for --multi-record stdin vs normal json"
     echo
 else
-    echo "FAILED to detect breakage when feeding normal multi-record json doc to stdin with --multi-record (expecting one json doc per line), returned unexpected exit code $exitcode"
+    echo "FAILED to detect breakage when feeding normal json doc to stdin with --multi-record (expecting one json doc per line), returned unexpected exit code $exitcode"
     exit 1
 fi
 
+echo "checking multirecord json stdin breakage without using --multi-record switch"
+set +e
+./validate_json.py - < "$data_dir/multirecord.json"
+exitcode=$?
+set -e
+if [ $exitcode = 2 ]; then
+    echo "successfully detected breakage for multirecord json on stdin without using --multi-record switch"
+    echo
+else
+    echo "FAILED to detect breakage when feeding multirecord json to stdin without using --multi-record, returned unexpected exit code $exitcode"
+    exit 1
+fi
+
+# ==================================================
+hr2
 echo blah > "$broken_dir/blah.json"
 check_broken "$broken_dir/blah.json"
 
 check_broken "$data_dir/single_quotes.notjson"
-check_broken "$data_dir/single_quotes_multirecord.notjson"
-check_broken "$data_dir/single_quotes_multirecord_embedded_double_quotes.notjson"
-check_broken "$data_dir/single_quotes_multirecord_embedded_double_quotes_unescaped.notjson"
+check_broken "$data_dir/multirecord_single_quotes.notjson"
+check_broken "$data_dir/multirecord_single_quotes_embedded_double_quotes.notjson"
+check_broken "$data_dir/multirecord_single_quotes_embedded_double_quotes_unescaped.notjson"
 
+# ==================================================
+hr2
 echo "checking --permit-single-quotes mode works"
 ./validate_json.py -s "$data_dir/single_quotes.notjson"
 echo
@@ -139,36 +149,69 @@ echo "checking --permit-single-quotes mode works with embedded double quotes"
 echo
 
 echo "checking --permit-single-quotes mode works with unescaped embedded double quotes"
-./validate_json.py -s "$data_dir/single_quotes_embedded_double_quotes.notjson"
+./validate_json.py -s "$data_dir/single_quotes_embedded_double_quotes_unescaped.notjson"
 echo
 
-echo "checking --permit-single-quotes mode works with multirecord"
-./validate_json.py -s "$data_dir/single_quotes_multirecord.notjson" -m
+# ==================================================
+hr2
+echo "checking --permit-single-quotes mode works with multirecord single quoted json"
+./validate_json.py -s "$data_dir/multirecord_single_quotes.notjson" -m
 echo
 
-echo "checking --permit-single-quotes mode works with multirecord"
-./validate_json.py -s "$data_dir/single_quotes_multirecord_embedded_double_quotes.notjson" -m
+echo "checking --permit-single-quotes mode infers multirecord single quoted json"
+./validate_json.py -s "$data_dir/multirecord_single_quotes.notjson"
 echo
 
-echo "checking --permit-single-quotes mode works with multirecord"
-./validate_json.py -s "$data_dir/single_quotes_multirecord_embedded_double_quotes_unescaped.notjson" -m
+# ==================================================
+hr2
+echo "checking --permit-single-quotes mode works with multirecord single quoted json with embedded double quotes"
+./validate_json.py -s "$data_dir/multirecord_single_quotes_embedded_double_quotes.notjson" -m
 echo
 
-echo "checking --permit-single-quotes mode works and auto retries to succeed with multirecord"
-./validate_json.py -s "$data_dir/single_quotes_multirecord.notjson"
+echo "checking --permit-single-quotes mode infers multirecord single quoted json with embedded double quotes"
+./validate_json.py -s "$data_dir/multirecord_single_quotes_embedded_double_quotes.notjson"
 echo
 
-echo "checking --permit-single-quotes mode works and auto retries to succeed with multirecord with embedded double quotes"
-./validate_json.py -s "$data_dir/single_quotes_multirecord_embedded_double_quotes.notjson"
+# ==================================================
+hr2
+echo "checking --permit-single-quotes mode works with multirecord json with unescaped embedded double quotes"
+./validate_json.py -s "$data_dir/multirecord_single_quotes_embedded_double_quotes_unescaped.notjson" -m
 echo
 
-echo "checking --permit-single-quotes mode works and auto retries to succeed with multirecord with unescaped embedded double quotes"
-./validate_json.py -s "$data_dir/single_quotes_multirecord_embedded_double_quotes_unescaped.notjson"
+echo "checking --permit-single-quotes mode infers multirecord single quoted json with unescaped embedded double quotes"
+./validate_json.py -s "$data_dir/multirecord_single_quotes_embedded_double_quotes_unescaped.notjson"
+echo
+
+# ==================================================
+hr2
+echo "checking --permit-single-quotes mode works with multirecord single quoted json with mixed quoting"
+./validate_json.py -s "$data_dir/multirecord_single_double_mixed_quotes.notjson" -m
+echo
+
+echo "checking --permit-single-quotes mode infers multirecord single quoted json with mixed quoting"
+./validate_json.py -s "$data_dir/multirecord_single_double_mixed_quotes.notjson"
+echo
+
+echo "checking --permit-single-quotes mode works with multirecord single quoted json with mixed quoting (should result in a WARNING message)"
+./validate_json.py -s "$data_dir/multirecord_single_double_mixed_quotes.notjson" -m 2>&1 |
+    grep -q WARNING &&
+        echo "Found warning message" ||
+            { echo "failed to raise a WARNING message for mixed quoting"; exit 1; }
+echo
+
+echo "checking --permit-single-quotes mode infers multirecord single quoted json with mixed quoting (should result in a WARNING message)"
+./validate_json.py -s "$data_dir/multirecord_single_double_mixed_quotes.notjson" 2>&1 |
+    grep -q WARNING &&
+        echo "Found warning message" ||
+            { echo "failed to raise a WARNING message for mixed quoting"; exit 1; }
 echo
 
 # ============================================================================ #
 #                          Print Mode Passthrough Tests
 # ============================================================================ #
+hr2
+echo "# Print Mode Passthrough Tests"
+hr2
 
 echo "testing print mode"
 [ "$(./validate_json.py -p "$data_dir/test.json" | cksum)" = "$(cksum < "$data_dir/test.json")" ] || { echo "print test failed!"; exit 1; }
@@ -186,28 +229,34 @@ echo "successfully passed test of print mode failure"
 echo
 
 echo "testing print mode with multi-record"
-[ "$(./validate_json.py -mp "$data_dir/multirecord.json" | cksum)" = "$(cksum < "$data_dir/multirecord.json")" ] || { echo "print multi-record test failed!"; exit 1; }
+[ "$(./validate_json.py -mp "$data_dir/multirecord.json" | cksum)" = "$(cksum < "$data_dir/multirecord.json")" ] ||
+    { echo "print multi-record test failed!"; exit 1; }
 echo "successfully passed out multi-record json to stdout"
 echo
 
 echo "testing print mode with --permit-single-quotes"
-[ "$(./validate_json.py -sp "$data_dir/single_quotes.notjson" | cksum)" = "$(cksum < "$data_dir/single_quotes.notjson")" ] || { echo "print single quote json test failed!"; exit 1; }
+[ "$(./validate_json.py -sp "$data_dir/single_quotes.notjson" | cksum)" = "$(cksum < "$data_dir/single_quotes.notjson")" ] ||
+    { echo "print single quote json test failed!"; exit 1; }
 echo
 
 echo "testing print mode with --permit-single-quotes multirecord"
-[ "$(./validate_json.py -sp "$data_dir/single_quotes_multirecord.notjson" | cksum)" = "$(cksum < "$data_dir/single_quotes_multirecord.notjson")" ] || { echo "print single quote multirecord json test failed!"; exit 1; }
+[ "$(./validate_json.py -sp "$data_dir/multirecord_single_quotes.notjson" | cksum)" = "$(cksum < "$data_dir/multirecord_single_quotes.notjson")" ] ||
+    { echo "print single quote multirecord json test failed!"; exit 1; }
 echo
 
 echo "testing print mode with --permit-single-quotes multirecord with embedded double quotes"
-[ "$(./validate_json.py -sp "$data_dir/single_quotes_multirecord.notjson" | cksum)" = "$(cksum < "$data_dir/single_quotes_multirecord.notjson")" ] || { echo "print single quote multirecord json with embedded double quotes test failed!"; exit 1; }
+[ "$(./validate_json.py -sp "$data_dir/multirecord_single_quotes_embedded_double_quotes.notjson" | cksum)" = "$(cksum < "$data_dir/multirecord_single_quotes_embedded_double_quotes.notjson")" ] ||
+    { echo "print single quote multirecord json with embedded double quotes test failed!"; exit 1; }
 echo
 
 echo "testing print mode with --permit-single-quotes multirecord with unescaped embedded double quotes"
-[ "$(./validate_json.py -sp "$data_dir/single_quotes_multirecord_embedded_double_quotes.notjson" | cksum)" = "$(cksum < "$data_dir/single_quotes_multirecord_embedded_double_quotes.notjson")" ] || { echo "print single quote multirecord json with unescaped embedded double quotes test failed!"; exit 1; }
+[ "$(./validate_json.py -sp "$data_dir/multirecord_single_quotes_embedded_double_quotes_unescaped.notjson" | cksum)" = "$(cksum < "$data_dir/multirecord_single_quotes_embedded_double_quotes_unescaped.notjson")" ] ||
+    { echo "print single quote multirecord json with unescaped embedded double quotes test failed!"; exit 1; }
 echo
 
 echo
 # ============================================================================ #
+hr2
 
 echo '{ "name": "hari" ' > "$broken_dir/missing_end_quote.json"
 check_broken "$broken_dir/missing_end_quote.json"
