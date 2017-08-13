4ff97656f6acfd8bba91f6bb887f0f2eaf8daa0a
Hari Sekhon <harisekhon@gmail.com>
2017-08-13 18:08:37 +0100

    updated test_validate_yaml.sh

--- tests/test_validate_yaml.sh
+++ tests/test_validate_yaml.sh
@@ -70,10 +70,11 @@ echo
 
 echo "Now trying non-yaml files to detect successful failure:"
 check_broken(){
-    filename="$1"
-    expected_exitcode="${2:-2}"
+    local filename="$1"
+    local expected_exitcode="${2:-2}"
+    local options="${@:3}"
     set +e
-    ./validate_yaml.py -t 1 "$filename" ${@:3}
+    ./validate_yaml.py -t 1 $options "$filename"
     exitcode=$?
     set -e
     if [ $exitcode = $expected_exitcode ]; then
