19641b0009ad328d154fa05f663f7d653f5a138f
Hari Sekhon <harisekhon@gmail.com>
2017-08-13 18:07:28 +0100

    updated test_validate_csv.sh

--- tests/test_validate_csv.sh
+++ tests/test_validate_csv.sh
@@ -74,7 +74,7 @@ check_broken(){
     local expected_exitcode="${2:-2}"
     local options="${@:3}"
     set +e
-    ./validate_csv.py -vvv -t 1 $options "$filename"
+    ./validate_csv.py -t 1 $options "$filename"
     exitcode=$?
     set -e
     if [ $exitcode = $expected_exitcode ]; then
