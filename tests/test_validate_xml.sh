a9ca64b6e0bf3fe5116b1a1b116c4d04f19eb326
Hari Sekhon <harisekhon@gmail.com>
2017-08-13 18:08:04 +0100

    updated test_validate_xml.sh

--- tests/test_validate_xml.sh
+++ tests/test_validate_xml.sh
@@ -68,10 +68,11 @@ echo
 
 echo "Now trying non-xml files to detect successful failure:"
 check_broken(){
-    filename="$1"
-    expected_exitcode="${2:-2}"
+    local filename="$1"
+    local expected_exitcode="${2:-2}"
+    local options="${@:3}"
     set +e
-    ./validate_xml.py "$filename" ${@:3}
+    ./validate_xml.py $options "$filename"
     exitcode=$?
     set -e
     if [ $exitcode = $expected_exitcode ]; then
