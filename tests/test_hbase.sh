0361b8e7932e9ff80e8e2e67630d60f5ba50affd
Hari Sekhon <harisekhon@gmail.com>
2017-08-16 20:11:08 +0100

    updated test_hbase.sh

--- tests/test_hbase.sh
+++ tests/test_hbase.sh
@@ -16,12 +16,12 @@
 
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
-
 srcdir2="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$srcdir2/.."
 
 . "$srcdir2/utils.sh"
+. "$srcdir2/../bash-tools/docker.sh"
 
 srcdir="$srcdir2"
 
@@ -41,25 +41,19 @@ export ZOOKEEPER_PORT=2181
 export HBASE_PORTS="$ZOOKEEPER_PORT $HBASE_STARGATE_PORT 8085 $HBASE_THRIFT_PORT 9095 16000 16010 16201 16301"
 export HBASE_TEST_PORTS="$ZOOKEEPER_PORT $HBASE_THRIFT_PORT"
 
-#export HBASE_VERSIONS="${@:-0.96 0.98 1.0 1.1 1.2}"
+#export HBASE_VERSIONS="${@:-0.96 0.98 1.0 1.1 1.2 latest}"
 # don't work
 #export HBASE_VERSIONS="0.98 0.96"
-export HBASE_VERSIONS="${@:-1.0 1.1 1.2}"
+export HBASE_VERSIONS="${@:-1.0 1.1 1.2 latest}"
 
-export DOCKER_IMAGE="harisekhon/hbase-dev"
-export DOCKER_CONTAINER="hbase-test-pytools"
+check_docker_available
 
-export MNTDIR=/pytools
-
-if ! is_docker_available; then
-    echo "WARNING: Docker not available, skipping HBase checks"
-    exit 0
-fi
+export MNTDIR="/pytools"
 
 startupwait=50
 
 docker_exec(){
-    docker exec -i "$DOCKER_CONTAINER" /bin/bash <<-EOF
+    docker exec -i docker_hbase_1 /bin/bash <<-EOF
     export JAVA_HOME=/usr
     $MNTDIR/$@
 EOF
@@ -67,15 +61,20 @@ EOF
 
 test_hbase(){
     local version="$1"
-    hr
-    echo "Setting up HBase $version test container"
-    hr
-    local DOCKER_OPTS="-v $srcdir/..:$MNTDIR"
-    launch_container "$DOCKER_IMAGE:$version" "$DOCKER_CONTAINER" 2181 8080 8085 9090 9095 16000 16010 16201 16301
-    when_ports_available $startupwait $HBASE_HOST $HBASE_TEST_PORTS
+    section2 "Setting up HBase $version test container"
+    #local DOCKER_OPTS="-v $srcdir/..:$MNTDIR"
+    #launch_container "$DOCKER_IMAGE:$version" "$DOCKER_CONTAINER" 2181 8080 8085 9090 9095 16000 16010 16201 16301
+    VERSION="$version" docker-compose up -d
+    local HBASE_STARGATE_PORT="`docker-compose port "$DOCKER_SERVICE" "$HBASE_STARGATE_PORT" | sed 's/.*://'`"
+    local HBASE_THRIFT_PORT="`docker-compose port "$DOCKER_SERVICE" "$HBASE_THRIFT_PORT" | sed 's/.*://'`"
+    local ZOOKEEPER_PORT="`docker-compose port "$DOCKER_SERVICE" "$ZOOKEEPER_PORT" | sed 's/.*://'`"
+    #hbase_ports=`{ for x in $HBASE_PORTS; do docker-compose port "$DOCKER_SERVICE" "$x"; done; } | sed 's/.*://'`
+    hbase_ports="$HBASE_STARGATE_PORT $HBASE_THRIFT_PORT $ZOOKEEPER_PORT"
+    when_ports_available "$startupwait" "$HBASE_HOST" $hbase_ports
+    #when_ports_available $startupwait $HBASE_HOST $HBASE_TEST_PORTS
     echo "setting up test tables"
-    uniq_val=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | head -c32 || :)
-    docker exec -i "$DOCKER_CONTAINER" /bin/bash <<-EOF
+    uniq_val=$(< /dev/urandom tr -dc 'a-zA-Z0-9' 2>/dev/null | head -c32 || :)
+    docker-compose exec "$DOCKER_SERVICE" /bin/bash <<-EOF
         export JAVA_HOME=/usr
         /hbase/bin/hbase shell <<-EOF2
             create 't1', 'cf1', { 'REGION_REPLICATION' => 1 }
@@ -88,10 +87,13 @@ test_hbase(){
 EOF2
         hbase org.apache.hadoop.hbase.util.RegionSplitter UniformSplitTable UniformSplit -c 100 -f cf1
         hbase org.apache.hadoop.hbase.util.RegionSplitter HexStringSplitTable HexStringSplit -c 100 -f cf1
+        # the above may fail, ensure we continue to try the tests
+        exit 0
 EOF
     if [ -n "${NOTESTS:-}" ]; then
         return
     fi
+    export HBASE_THRIFT_SERVER_PORT="$HBASE_THRIFT_PORT"
     hr
     ./hbase_generate_data.py -n 10
     hr
@@ -199,7 +201,8 @@ EOF
     ./hbase_calculate_table_row_key_distribution.py -T HexStringSplitTable
     hr
 
-    delete_container
+    #delete_container
+    docker-compose down
     echo
 }
 
