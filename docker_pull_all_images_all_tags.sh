9382e4adb2d1f77e01fca38536c6d20c6f3f15d9
Hari Sekhon <harisekhon@gmail.com>
2017-08-29 15:00:04 +0200

    added docker_pull_all_images_all_tags.sh

---
+++ docker_pull_all_images_all_tags.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2017-08-29 14:57:23 +0200 (Tue, 29 Aug 2017)
+#
+#  https://github.com/harisekhon/pytools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+for docker_image in $(./dockerhub_search.py -q harisekhon); do
+    for tag in $(./dockerhub_show_tags.py -q $docker_image); do
+        docker pull $docker_image:$tag
+    done
+done
