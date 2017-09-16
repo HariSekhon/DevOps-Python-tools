690de55572db358a95c9c9b4ed2af8b5604d623c
Hari Sekhon <harisekhon@gmail.com>
2017-09-16 19:46:53 +0100

    added sample_file() and check_broken_sample_files() functions

--- tests/utils.sh
+++ tests/utils.sh
@@ -34,3 +34,37 @@ export SPARK_HOME="$(ls -d tests/spark-*-bin-hadoop* | head -n 1)"
 . "$srcdir/excluded.sh"
 
 . "$srcdir/check.sh"
+
+# echo sample files except those with extensions given as args
+sample_files(){
+    local data_dir="$srcdir2/data"
+    for filename in \
+        "$data_dir/add_ou.ldif" \
+        "$data_dir/multirecord.json" \
+        "$data_dir/simple.xml" \
+        "$data_dir/test.csv" \
+        "$data_dir/test.ini" \
+        "$data_dir/test.json" \
+        "$data_dir/test.yaml" \
+        "$data_dir/../../README.md" \
+        ; do
+        local excluded=0
+        for ext in $@; do
+            if [ "${filename##*.}" = "$ext" ]; then
+                excluded=1
+                break
+            fi
+        done
+        if [ $excluded = 0 ]; then
+            echo "$filename"
+        fi
+    done
+}
+
+check_broken_sample_files(){
+    echo "Now checking non $@ files to detect successful failure:"
+    echo
+    for filename in $(sample_files $@); do
+        check_broken "$filename"
+    done
+}
