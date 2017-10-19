835f00aa2974660aa65e386e16d9fcb528e8b4d6
Hari Sekhon <harisekhon@gmail.com>
2017-10-19 10:42:35 +0200

    update tests/test_hadoop.sh

--- tests/test_hadoop.sh
+++ tests/test_hadoop.sh
@@ -63,18 +63,10 @@ test_hadoop(){
     section2 "Setting up Hadoop $version test container"
     VERSION="$version" docker-compose up -d
     echo "getting Hadoop dynamic port mappings:"
-    printf "getting HDFS NN port => "
-    export HADOOP_NAMENODE_PORT="`docker-compose port "$DOCKER_SERVICE" "$HADOOP_NAMENODE_PORT_DEFAULT" | sed 's/.*://'`"
-    echo "$HADOOP_NAMENODE_PORT"
-    printf "getting HDFS DN port => "
-    export HADOOP_DATANODE_PORT="`docker-compose port "$DOCKER_SERVICE" "$HADOOP_DATANODE_PORT_DEFAULT" | sed 's/.*://'`"
-    echo "$HADOOP_DATANODE_PORT"
-    printf  "getting Yarn RM port => "
-    export HADOOP_YARN_RESOURCE_MANAGER_PORT="`docker-compose port "$DOCKER_SERVICE" "$HADOOP_YARN_RESOURCE_MANAGER_PORT_DEFAULT" | sed 's/.*://'`"
-    echo "$HADOOP_YARN_RESOURCE_MANAGER_PORT"
-    printf "getting Yarn NM port => "
-    export HADOOP_YARN_NODE_MANAGER_PORT="`docker-compose port "$DOCKER_SERVICE" "$HADOOP_YARN_NODE_MANAGER_PORT_DEFAULT" | sed 's/.*://'`"
-    echo "$HADOOP_YARN_NODE_MANAGER_PORT"
+    docker_compose_port HADOOP_NAMENODE_PORT "HDFS NN"
+    docker_compose_port HADOOP_YARN_RESOURCE_MANAGER_PORT "HDFS RM"
+    docker_compose_port HADOOP_DATANODE_PORT "HDFS DN"
+    docker_compose_port HADOOP_YARN_NODE_MANAGER_PORT "HDFS NM"
     export HADOOP_PORTS="$HADOOP_NAMENODE_PORT $HADOOP_DATANODE_PORT $HADOOP_YARN_RESOURCE_MANAGER_PORT $HADOOP_YARN_NODE_MANAGER_PORT"
     hr
     when_ports_available "$HADOOP_HOST" $HADOOP_PORTS
