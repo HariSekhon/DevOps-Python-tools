875ff9b2b410493e179523669a7a0c61bc062509
Hari Sekhon <harisekhon@gmail.com>
2018-07-04 00:27:04 +0100

    updated hbase_calculate_table_region_requests_per_sec.py

--- hbase_calculate_table_region_requests_per_sec.py
+++ hbase_calculate_table_region_requests_per_sec.py
@@ -37,6 +37,7 @@ import os
 import re
 import sys
 import traceback
+import requests
 libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'pylib'))
 sys.path.append(libdir)
 try:
@@ -48,7 +49,7 @@ except ImportError as _:
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.1'
+__version__ = '0.2'
 
 
 class HBaseCalculateTableRegionsRequestsPerSec(CLI):
@@ -59,9 +60,9 @@ class HBaseCalculateTableRegionsRequestsPerSec(CLI):
         # Python 3.x
         # super().__init__()
         self.host_list = []
-        self.port = 16301
+        self.port = 16030
         self.table = None
-        self.namespace = '.*'
+        self.namespace = 'default'
         self.timeout_default = 300
         #self.re_hex = re.compile('([a-f]+)') # to convert to uppercase later for aesthetics
         self._regions = None
@@ -69,7 +70,7 @@ class HBaseCalculateTableRegionsRequestsPerSec(CLI):
     def add_options(self):
         self.add_opt('-P', '--port', default=self.port, help='HBase RegionServer port (default: {})'.format(self.port))
         self.add_opt('-T', '--table', help='Table name')
-        #self.add_opt('-N', '--namespace', default='Default', help='Namespace (default: Default)')
+        self.add_opt('-N', '--namespace', default=self.namespace, help='Namespace (default: {})'.format(self.namespace))
 
     def process_args(self):
         #log.setLevel(logging.INFO)
@@ -82,25 +83,15 @@ class HBaseCalculateTableRegionsRequestsPerSec(CLI):
         validate_port(self.port)
         self.table = self.get_opt('table')
         validate_chars(self.table, 'hbase table', 'A-Za-z0-9:._-')
-        #self.namespace = self.get_opt('namespace')
-        #validate_chars(self.namespace, 'hbase namespace', 'A-Za-z0-9:._-')
+        self.namespace = self.get_opt('namespace')
+        validate_chars(self.namespace, 'hbase namespace', 'A-Za-z0-9:._-')
 
     def run(self):
-#        region_header = 'Region'
-#        start_key_header = 'Start Key'
-#        end_key_header = 'End Key'
-#        server_header = 'Server (host:port)'
-#        separator = '    '
-#        region_width = len(self.region_header)
-#        start_key_width = len(self.start_key_header)
-#        end_key_width = len(self.end_key_header)
-#        server_width = len(self.server_header)
-#        total_width = (self.region_width + self.server_width +
-#                            self.start_key_width + self.end_key_width +
-#                            len(3 * self.separator))
-        # use built-in pylib's request handler and CLI exception handlers error catching, keep code here short
         for host in self.host_list:
-            self.print_region_stats(host)
+            try:
+                self.print_region_stats(host)
+            except (UnknownError, requests.RequestException) as _:
+                print("ERROR querying JMX stats for host '{}': {}".format(host, _))
 
     def print_region_stats(self, host):
         req = RequestHandler().get('http://{host}:{port}/jmx'.format(host=host, port=self.port))
@@ -112,16 +103,18 @@ class HBaseCalculateTableRegionsRequestsPerSec(CLI):
                 uptime = int(bean['Uptime'] / 1000)
                 break
         if not uptime:
-            raise UnknownError('failed to find uptime in JMX stats. {}'.format(support_msg_api()))
+            raise UnknownError("failed to find uptime in JMX stats for host '{}'. {}".format(host, support_msg_api()))
 
-        region_regex = re.compile('^Namespace_{namespace}_table_{table}_region_(.+)_metric_.*RequestCount'\
+        region_regex = re.compile('^Namespace_{namespace}_table_{table}_region_(.+)_metric_(.+)RequestCount'\
                                   .format(namespace=self.namespace, table=self.table))
         for bean in beans:
             if bean['name'] == 'Hadoop:service=HBase,name=RegionServer,sub=Regions':
-                for key in bean:
+                for key in sorted(bean):
                     match = region_regex.match(key)
                     if match:
-                        print('{}\t{:.1f}'.format(match.group(1), bean[key] / uptime))
+                        region = match.group(1)
+                        metric = match.group(2)
+                        print('{:20s}\t{:20s}\t\t{:10s}\t{:.1f}'.format(host, region, metric, bean[key] / uptime))
 
     # some extra effort to make it look the same as HBase presents it as
     #def encode_char(self, char):
@@ -130,6 +123,7 @@ class HBaseCalculateTableRegionsRequestsPerSec(CLI):
     #    _ = '{0:#0{1}x}'.format(ord(char), 4).replace('0x', '\\x')
     #    _ = self.re_hex.sub(lambda x: x.group(1).upper(), _)
     #    return _
+
 
 if __name__ == '__main__':
     HBaseCalculateTableRegionsRequestsPerSec().main()
