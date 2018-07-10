9b581e3dee297dffd3d581080bf3236550db1cc7
Hari Sekhon <harisekhon@gmail.com>
2018-07-10 20:04:03 +0100

    updated hbase_region_requests.py

--- hbase_region_requests.py
+++ hbase_region_requests.py
@@ -53,11 +53,11 @@ __author__ = 'Hari Sekhon'
 __version__ = '0.5'
 
 
-class HBaseRegionsRequestsPerSec(CLI):
+class HBaseRegionsRequests(CLI):
 
     def __init__(self):
         # Python 2.x
-        super(HBaseRegionsRequestsPerSec, self).__init__()
+        super(HBaseRegionsRequests, self).__init__()
         # Python 3.x
         # super().__init__()
         self.host_list = []
@@ -229,4 +229,4 @@ class HBaseRegionsRequestsPerSec(CLI):
 
 
 if __name__ == '__main__':
-    HBaseRegionsRequestsPerSec().main()
+    HBaseRegionsRequests().main()
