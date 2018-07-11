8cc2b4356e4ed7778ffe9f633edbb3c847cfd659
Hari Sekhon <harisekhon@gmail.com>
2018-07-11 19:35:44 +0100

    added hbase_uncompacted_regions.sh

---
+++ hbase_uncompacted_regions.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2018-07-11 19:26:21 +0100 (Wed, 11 Jul 2018)
+#
+#  https://github.com/harisekhon/pytools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+usage(){
+    echo "usage: $0 <list of regionservers>"
+    exit 1
+}
+
+port="${REGIONSERVER_PORT:-16030}"
+
+if [ $# -lt 1 ]; then
+    usage
+fi
+
+for server in $@; do
+    curl -s $server:$port/jmx |
+    grep 'compactionsCompletedCount"[[:space:]]*:[[:space:]]*0,$' |
+    sed 's/.*_table_//; s/_region_/ /; s/_metric_.*//' |
+    column -t
+done
