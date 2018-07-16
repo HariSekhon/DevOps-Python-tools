add400488fabf90ce633e527286e0055f15a8f16
Hari Sekhon <harisekhon@gmail.com>
2018-07-16 22:27:22 +0100

    updated ambari_ams_list_hosts.sh

--- ambari_ams_list_hosts.sh
+++ ambari_ams_list_hosts.sh
@@ -24,10 +24,10 @@ usage: ${0##*/} <ambari_metrics_collector_host>
     exit 1
 fi
 
-ambari_host="$1"
-ambari_port="${AMBARI_PORT:6188}"
+ams_host="$1"
+ams_port="${AMBARI_METRICS_COLLECTOR_PORT:-${AMBARI_PORT:6188}}"
 
-curl "$ambari_host:$ambari_port/ws/v1/timeline/metrics/hosts" |
+curl "$ams_host:$ams_port/ws/v1/timeline/metrics/hosts" |
 python -m json.tool |
 egrep '^[[:space:]]*".+":[[:space:]]* \[[[:space:]]*$' |
 sed 's/"//g;s/:.*//;s/[[:space:]]*//g' |
