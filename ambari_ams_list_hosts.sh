2ea9d320823036d13406285397f3ca853f691417
Hari Sekhon <harisekhon@gmail.com>
2018-07-16 22:19:23 +0100

    added ambari_ams_list_hosts.sh

---
+++ ambari_ams_list_hosts.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+#  vim:ts=4:sts=4:sw=4:et
+#
+#  Author: Hari Sekhon
+#  Date: 2018-07-16 22:14:34 +0100 (Mon, 16 Jul 2018)
+#
+#  https://github.com/harisekhon/pytools
+#
+#  License: see accompanying Hari Sekhon LICENSE file
+#
+#  If you're using my code you're welcome to connect with me on LinkedIn and optionally send me feedback to help steer this or other code I publish
+#
+#  https://www.linkedin.com/in/harisekhon
+#
+
+set -euo pipefail
+[ -n "${DEBUG:-}" ] && set -x
+
+if [ $# != 1 ]; then
+    echo "Lists all hosts in Ambari Metrics Collector service via the API
+
+usage: ${0##*/} <ambari_host>
+"
+    exit 1
+fi
+
+ambari_host="$1"
+ambari_port="${AMBARI_PORT:6188}"
+
+curl "$ambari_host:$ambari_port/ws/v1/timeline/metrics/hosts" |
+python -m json.tool |
+egrep '^[[:space:]]*".+":[[:space:]]* \[[[:space:]]*$' |
+sed 's/"//g;s/:.*//;s/[[:space:]]*//g' |
+grep -v fakehostname |
+sort
