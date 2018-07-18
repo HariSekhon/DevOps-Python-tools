6670b023a9e9e02c5d120b621c1b13037b24afe8
Hari Sekhon <harisekhon@gmail.com>
2018-07-18 21:50:05 +0100

    updated ambari_ams_list_metrics.sh

--- ambari_ams_list_metrics.sh
+++ ambari_ams_list_metrics.sh
@@ -18,31 +18,60 @@
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 
-usage(){
-    echo "Lists all hosts in Ambari Metrics Collector service via the API
+ams_host="${AMBARI_METRICS_COLLECTOR_HOST:-${AMBARI_HOST:-localhost}}"
+ams_port="${AMBARI_METRICS_COLLECTOR_PORT:-${AMBARI_PORT:-6188}}"
 
-usage: ${0##*/} <ambari_metrics_collector_host>
+usage(){
+    if [ -n "$*" ]; then
+        echo "$@"
+        echo
+    fi
+    echo "Lists all metrics in Ambari Metrics Collector service via the API
+
+usage: ${0##*/} [options]
+
+-H  --host      Ambari Metrics Collector host (default: localhost, \$AMBARI_METRICS_COLLECTOR_HOST, \$AMBARI_HOST)
+-P  --port      Ambari Metrics Collector port (default: 6188, \$AMBARI_METRICS_COLLECTOR_PORT, \$AMBARI_PORT)
 "
     exit 1
 }
 
-if [ $# != 1 ]; then
-    usage
-fi
-
-for arg in $@; do
-    case $arg in
-        -*) usage
-            ;;
+until [ $# -lt 1 ]; do
+    case $1 in
+    -H|--host)  ams_host="${2:-}"
+                shift
+                ;;
+    -P|--port)  ams_host="${2:-}"
+                shift
+                ;;
+    -h|--help)  usage
+                ;;
+            *)  usage "unknown argument: $1"
+                ;;
     esac
+    shift
 done
 
-ams_host="$1"
-ams_port="${AMBARI_METRICS_COLLECTOR_PORT:-${AMBARI_PORT:6188}}"
+if [ -z "$ams_host" ]; then
+    usage "--host not defined"
+elif [ -z "$ams_port" ]; then
+    usage "--port not defined"
+fi
+
+check_bin(){
+    local bin="$1"
+    if ! which $bin &>/dev/null; then
+        echo "$bin command not found in \$PATH ($PATH)"
+        exit 1
+    fi
+}
+check_bin curl
+check_bin python
 
 curl -s "$ams_host:$ams_port/ws/v1/timeline/metrics/metadata" |
 python -m json.tool |
 grep metricname |
 awk '{print $2}' |
 sed 's/"//g;s/,$//' |
-sort -u
+sort -u ||
+    { echo "You probably specified the wrong --host/--port"; exit 2; }
