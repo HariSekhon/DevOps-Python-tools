0dda94df8ede96afcd50a97ffbe21da834779a9d
Hari Sekhon <harisekhon@gmail.com>
2018-07-18 21:22:41 +0100

    merged opentsdb metrics listing scripts in to one proper script

--- opentsdb_list_metrics.sh
+++ opentsdb_list_metrics.sh
@@ -13,19 +13,99 @@
 #  https://www.linkedin.com/in/harisekhon
 #
 
-# Quick script to list OpenTSDB metrics via local HBase Shell
-
-# Tested on OpenTSDB 2.3 on HBase 1.4
-
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 
-if ! which hbase &>/dev/null; then
-    echo "hbase command not found in \$PATH ($PATH)"
-    exit 1
-fi
+PATH="$PATH:/opt/hbase/bin:/hbase/bin"
 
-hbase shell <<< "scan 'tsdb-uid', { COLUMNS => 'name:metrics', VERSIONS => 1 }" 2>/dev/null |
-grep 'value=' |
-sed 's/^.*, value=//' |
-sort -u
+metrics="metrics"
+by_age=0
+
+usage(){
+    if [ -n "$@" ]; then
+        echo "$@"
+        echo
+    fi
+    cat <<EOF
+Quick script to list OpenTSDB metrics via local HBase Shell
+
+Metrics output - one metric per line
+
+Metrics By Age - output:
+
+<timestamp_epoch_millis>  <human_timestamp>   <metric>
+
+Tested on OpenTSDB 2.3 on HBase 1.4
+
+
+usage: ${0##*/} [options]
+
+--tagk      List tagk values (default lists metrics)
+--tagv      List tagv values (default lists metrics, mutually exclusive with --tagk)
+--by-age    List by age, oldest first with epoch, human time and metric columns
+
+EOF
+    exit 1
+}
+
+set_metric_type(){
+    if [ "$metrics" != "metrics" ]; then
+        usage "--tagk and --tagv are mutually exclusive!"
+    fi
+    metrics="$1"
+}
+
+until [ $# -lt 1 ]; do
+    case $1 in
+       --tagk)  set_metric_type tagk
+                ;;
+       --tagv)  set_metric_type tagv
+                ;;
+     --by-age)  by_age=1
+                ;;
+            *)  usage "unknown argument: $1"
+                ;;
+    esac
+    shift || :
+done
+ 
+check_bin(){
+    local bin="$1"
+    if ! which $bin &>/dev/null; then
+        echo "$bin command not found in \$PATH ($PATH)"
+        exit 1
+    fi
+}
+check_bin hbase
+check_bin python
+
+print_metrics(){
+    hbase shell <<< "scan 'tsdb-uid', { COLUMNS => 'name:$metrics', VERSIONS => 1 }" 2>/dev/null |
+    grep 'value=' |
+    sed 's/^.*, value=//' |
+    sort -u
+}
+
+print_metrics_by_age(){
+    # hackish but convenient - forking to the date command thousands or hundreds of thousands of times can take hours, python takes 10 secs even for 250,000+ metrics
+    tmp_python_script=$(mktemp)
+    cat > "$tmp_python_script" <<EOF
+import sys, time
+for line in sys.stdin:
+    (ts, metric) = line.split()
+    print "{}\t{}\t{}".format(ts, time.strftime("%F %T", time.localtime(int(ts)/1000)), metric)
+EOF
+    trap "rm '$tmp_python_script'" EXIT
+
+    hbase shell <<< "scan 'tsdb-uid', { COLUMNS => 'name:$metrics', VERSIONS => 1 }" 2>/dev/null |
+    grep 'value=' |
+    sed 's/^.*, timestamp=//; s/, value=/ /' |
+    python "$tmp_python_script" |
+    sort -k1n
+}
+
+if [ $by_age = 0 ]; then
+    print_metrics
+else
+    print_metrics_by_age
+fi
