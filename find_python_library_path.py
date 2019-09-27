657c2999728bfacffae182f3bc85e318e582e3a4
Hari Sekhon <harisekhon@gmail.com>
2019-09-27 12:01:32 +0100

    updated find_python_library_path.py

--- find_python_library_path.py
+++ find_python_library_path.py
@@ -20,6 +20,8 @@ Simply tool to print the local path to one or more libraries given as arguments
 
 Useful for finding where things are installed on different operating systems like Mac vs Linux
 
+Tested on Python 2.7 and Python 3.x on Mac and Linux
+
 """
 
 from __future__ import absolute_import
@@ -41,27 +43,42 @@ def usage():
     print('usage: {} <library1> [<library2> <library3>...]'.format(os.path.basename(sys.argv[0])), file=sys.stderr)
     sys.exit(3)
 
+def get_module_location(module):
+    # pylint: disable=wrong-import-position,import-error
+    imported_module = __import__(module)
+    # sys doesn't have __file__ but it does have base_prefix
+    attribs = dir(imported_module)
+    # - looks like 'sys.base_prefix' / 'sys.base_exec_prefix' are only available in IPython
+    # - don't use 'executable' as it will point to a generic location eg. /usr/bin/python
+    #   or /usr/local/opt/python/bin/python3.7) instead of the actual library path
+    #   like /usr/local/Cellar/python/3.7.4_1/Frameworks/Python.framework/Versions/3.7
+    #potential_attribs = ['__file__', 'exec_prefix', 'base_prefix', 'base_exec_prefix']
+    potential_attribs = ['__file__', 'exec_prefix'] #, 'executable']
+    for potential_attrib in potential_attribs:
+        if potential_attrib in attribs:
+            value = getattr(imported_module, potential_attrib)
+            if potential_attrib == 'exec_prefix':
+                if not 'Python.framework' in value:
+                    continue
+            return value
+    # to account for sys not having __file__ attribute
+    if '__name__' in attribs and imported_module.__name__ == 'sys':
+        python_site_packages = 'python{}.{}/site-packages'\
+                               .format(sys.version_info.major, sys.version_info.minor)
+        for path in sys.path:
+            if path.endswith(python_site_packages):
+                value = path.rsplit('/site-packages')[0]
+                return value
+    raise AttributeError('could not find any attribute to determine location out of: {}'\
+                         .format(', '.join(potential_attribs)))
 
 def main():
     exitcode = 0
     for module in sys.argv[1:]:
         try:
-            # pylint: disable=wrong-import-position,import-error
-            imported = __import__(module)
-            # sys doesn't have __file__ but it does have base_prefix
-            attribs = dir(imported)
-            potential_attribs = ['__file__', 'base_prefix', 'base_exec_prefix', 'exec_prefix']
-            found = False
-            for _ in potential_attribs:
-                if _ in attribs:
-                    print(getattr(imported, _))
-                    found = True
-                    break
-            if not found:
-                raise AttributeError('could not find any attribute to determine location out of: {}'\
-                                     .format(', '.join(potential_attribs)))
+            print(get_module_location(module))
         except AttributeError as _:
-            print('attributes not found: {} - {}'.format(module, _), file=sys.stderr)
+            print('attributes not found in module: {} - {}'.format(module, _), file=sys.stderr)
             exitcode = max(1, exitcode)
         except ImportError as _:
             print('module not found: {} - {}'.format(module, _), file=sys.stderr)
