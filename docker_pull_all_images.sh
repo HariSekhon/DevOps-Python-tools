07db6eb81d6acd80e73a1004f462f2d4464f06f7
Hari Sekhon <harisekhon@gmail.com>
2019-10-03 16:22:42 +0100

    updated docker_pull_all_images.sh

--- docker_pull_all_images.sh
+++ docker_pull_all_images.sh
@@ -16,7 +16,7 @@
 set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 
-srcdir="$(dirname $0)"
+srcdir="$(dirname "$0")"
 
 for docker_image in $("$srcdir/dockerhub_search.py" -q harisekhon); do
     echo "docker pull $docker_image"
