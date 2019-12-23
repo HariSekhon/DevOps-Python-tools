27fae644a72b331dfa6d5950c9b618f6a1f518d5
Hari Sekhon <harisekhon@gmail.com>
2019-12-23 17:50:16 +0000

    updated hive_foreach_table.py

--- hive_foreach_table.py
+++ hive_foreach_table.py
@@ -16,7 +16,7 @@
 
 """
 
-Tool to connect to a HiveServer2 and execute a query for all tables in all databases,
+Tool to connect to a HiveServer2 / Impala node and execute a query for all tables in all databases,
 or only those matching given db / table regexes
 
 Useful for getting row counts of all tables or analyzing tables:
@@ -26,12 +26,17 @@ eg.
 hive_foreach_table.py --query 'SELECT COUNT(*) FROM {db}.{table}'
 hive_foreach_table.py --query 'ANALYZE TABLE {db}.{table} COMPUTE STATS'
 
+impala_foreach_table.py --query 'SELECT COUNT(*) FROM {db}.{table}'
+impala_foreach_table.py --query 'COMPUTE STATS {table}'
+
 or just for today's partition:
 
 hive_foreach_table.py --query "ANALYZE TABLE {db}.{table} PARTITION(date=$(date '+%Y-%m-%d')) COMPUTE STATS"
 
+impala_foreach_table.py --query "COMPUTE INCREMENTAL STATS {db}.{table} PARTITION(date=$(date '+%Y-%m-%d'))"
 
-Tested on CDH 5.10, Hive 1.1.0 with Kerberos
+
+Tested on CDH 5.10, Hive 1.1.0 and Impala 2.7.0 with Kerberos
 
 Due to a thrift / impyla bug this needs exactly thrift==0.9.3, see
 
@@ -68,18 +73,6 @@ __version__ = '0.2.0'
 logging.basicConfig()
 log = logging.getLogger(os.path.basename(sys.argv[0]))
 
-host_envs = [
-    'HIVESERVER2_HOST',
-    'HIVE_HOST',
-    'HOST'
-]
-
-port_envs = [
-    'HIVESERVER2_PORT',
-    'HIVE_PORT',
-    'PORT'
-]
-
 def getenvs(keys, default=None):
     for key in keys:
         value = os.getenv(key)
@@ -88,19 +81,46 @@ def getenvs(keys, default=None):
     return default
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Executes a SQL statement for each matching Hive table")
+    name = 'HiveServer2'
+    default_port = 10000
+    default_service_name = 'hive'
+    host_envs = [
+        'HIVESERVER2_HOST',
+        'HIVE_HOST',
+        'HOST'
+    ]
+    port_envs = [
+        'HIVESERVER2_PORT',
+        'HIVE_PORT',
+        'PORT'
+    ]
+
+    if 'impala' in sys.argv[0]:
+        name = 'Impala'
+        default_port = 21050
+        default_service_name = 'impala'
+        host_envs = [
+            'IMPALA_HOST',
+            'HOST'
+        ]
+        port_envs = [
+            'IMPALA_PORT',
+            'PORT'
+        ]
+    parser = argparse.ArgumentParser(description="Executes a SQL statement for each matching {} table".format(name))
     parser.add_argument('-H', '--host', default=getenvs(host_envs, socket.getfqdn()),\
-                        help='HiveServer2 host ' + \
+                        help='{} host '.format(name) + \
                              '(default: fqdn of local host, $' + ', $'.join(host_envs) + ')')
-    parser.add_argument('-P', '--port', type=int, default=getenvs(port_envs, 10000),
-                        help='HiveServer2 port (default: 10000, ' + ', $'.join(port_envs) + ')')
+    parser.add_argument('-P', '--port', type=int, default=getenvs(port_envs, default_port),
+                        help='{} port (default: {}, '.format(name, default_port) + \
+                                                                  ', $'.join(port_envs) + ')')
     parser.add_argument('-q', '--query', required=True, help='Query or statement to execute for each table' + \
             ' (replaces {db} and {table} in the query string with each table and its database)')
     parser.add_argument('-d', '--database', default='.*', help='Database regex (default: .*)')
     parser.add_argument('-t', '--table', default='.*', help='Table regex (default: .*)')
     parser.add_argument('-k', '--kerberos', action='store_true', help='Use Kerberos (you must kinit first)')
-    parser.add_argument('-n', '--krb5-service-name', default='hive',
-                        help='Service principal (default: \'hive\')')
+    parser.add_argument('-n', '--krb5-service-name', default=default_service_name,
+                        help='Service principal (default: {})'.format(default_service_name))
     parser.add_argument('-S', '--ssl', action='store_true', help='Use SSL')
     parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode')
     args = parser.parse_args()
