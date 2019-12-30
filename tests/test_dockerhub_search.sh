e45d8a1fd2b970855cf306d5218cf5a3d9d2fb61
Hari Sekhon <harisekhon@gmail.com>
2019-12-30 18:14:08 +0000

    updated test_dockerhub_search.sh

--- tests/test_dockerhub_search.sh
+++ tests/test_dockerhub_search.sh
@@ -28,7 +28,9 @@ section "Testing DockerHub Show Tags"
 check './dockerhub_search.py centos' "DockerHub Search for CentOS"
 check './dockerhub_search.py harisekhon' "DockerHub Search for harisekhon"
 check './dockerhub_search.py harisekhon -n 30' "DockerHub Search for harisekhon -n 30"
-check './dockerhub_search.py harisekhon/hadoop-dev | grep harisekhon/hadoop-dev' "DockerHub Search for harisekhon/hadoop-dev"
+# this no longer works, API must have changed
+#check './dockerhub_search.py harisekhon/hadoop-dev | grep harisekhon/hadoop-dev' "DockerHub Search for harisekhon/hadoop-dev"
+check './dockerhub_search.py hadoop-dev | grep harisekhon/hadoop-dev' "DockerHub Search for harisekhon/hadoop-dev"
 # causes IOError: [Errno 32] Broken pipe
 #unset PYTHONUNBUFFERED
 check '[ $(./dockerhub_search.py -q harisekhon | head -n 40 | tee /dev/stderr | grep "^harisekhon/[A-Za-z0-9_-]*$" | wc -l) = 40 ]' "DockerHub Search quiet mode for shell scripting"
