223e005b504a4d89698ed09fba5c7b4102d2b5f9
Hari Sekhon <harisekhon@gmail.com>
2020-01-13 11:14:22 +0000

    updated aws_users_unused_access_keys.py

--- aws_users_unused_access_keys.py
+++ aws_users_unused_access_keys.py
@@ -16,7 +16,7 @@
 
 """
 
-Find AWS IAM user access keys that haven't been used in N days or since created
+Find AWS IAM user access keys that haven't been used in N days and keys that have never been used
 
 Generates an IAM credential report, then parses it to determine the time since each user's password
 and access keys were last used
@@ -25,8 +25,7 @@ Requires iam:GenerateCredentialReport on resource: *
 
 Output:
 
-<user>  <access_key>  <unused_days>  <access_key_1_last_used_date>
-<user>  <access_key>  <unused_days>  <access_key_2_last_used_date>
+<user>  <access_key_num>  <status>  <unused_days>  <last_used_date>  <created_date>
 
 Uses the Boto python library, read here for the list of ways to configure your AWS credentials:
 
@@ -66,7 +65,7 @@ except ImportError as _:
     sys.exit(4)
 
 __author__ = 'Hari Sekhon'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 
 class AWSUnusedAccessKeys(CLI):
@@ -78,12 +77,15 @@ class AWSUnusedAccessKeys(CLI):
         # super().__init__()
         self.age = None
         self.now = None
+        self.only_active = False
         self.timeout_default = 300
         self.msg = 'AWSUnusedAccessKeys msg not defined'
 
     def add_options(self):
-        self.add_opt('-a', '--age', type=float, default=90,
-                     help='Show only access keys not used in the last N days (default: 90)')
+        self.add_opt('-a', '--age', type=float, default=30,
+                     help='Show only access keys not used in the last N days (default: 30)')
+        self.add_opt('-o', '--only-active', action='store_true',
+                     help='Only show access keys that are active')
 
     def process_args(self):
         self.no_args()
@@ -114,7 +116,11 @@ class AWSUnusedAccessKeys(CLI):
         headers = csvreader.next()
         log.debug('headers: %s', headers)
         assert headers[0] == 'user'
+        assert headers[8] == 'access_key_1_active'
+        assert headers[9] == 'access_key_1_last_rotated'
         assert headers[10] == 'access_key_1_last_used_date'
+        assert headers[13] == 'access_key_2_active'
+        assert headers[14] == 'access_key_2_last_rotated'
         assert headers[15] == 'access_key_2_last_used_date'
         self.now = datetime.utcnow()
         for row in csvreader:
@@ -123,27 +129,53 @@ class AWSUnusedAccessKeys(CLI):
     def process_user(self, row):
         log.debug('processing user: %s', row)
         user = row[0]
-        password_last_used = row[4]
-        access_key_1_last_used_date = row[10]
-        access_key_2_last_used_date = row[15]
-        log.debug('user: %s, password_last_used: %s, access_key_1_last_used_date: %s, access_key_2_last_used_date: %s',
-                  user, password_last_used, access_key_1_last_used_date, access_key_2_last_used_date)
-        key = 1
-        for _ in [access_key_1_last_used_date, access_key_2_last_used_date]:
-            if _ == 'N/A':
+        access_keys = {1:{}, 2:{}}
+        access_keys[1]['active'] = row[8]
+        access_keys[1]['last_used_date'] = row[10]
+        access_keys[1]['last_rotated'] = row[9]
+        access_keys[2]['active'] = row[13]
+        access_keys[2]['last_rotated'] = row[14]
+        access_keys[2]['last_used_date'] = row[15]
+        for key in [1, 2]:
+            active = access_keys[key]['active']
+            if not isinstance(active, bool):
+                assert active in ('true', 'false')
+                active = active.lower() == 'true'
+            created = access_keys[key]['last_rotated']
+            last_used = access_keys[key]['last_used_date']
+            log.debug('user: %s, key: %s, active: %s, created: %s, last_used_date: %s, ',
+                      user,
+                      key,
+                      active,
+                      created,
+                      last_used
+                     )
+            if not active and self.only_active:
+                continue
+            if last_used == 'N/A':
+                if created == 'N/A':
+                    continue
+                self.print_key(user, key, active, 'N/A', last_used, created)
                 continue
             # %z not working in Python 2.7 but we already know it's +00:00
-            _datetime = datetime.strptime(_.split('+')[0], '%Y-%m-%dT%H:%M:%S')
+            _datetime = datetime.strptime(last_used.split('+')[0], '%Y-%m-%dT%H:%M:%S')
             age_timedelta = self.now - _datetime.replace(tzinfo=None)
             age_days = int(floor(age_timedelta.total_seconds() / 86400.0))
             if age_days > self.age:
-                log.debug('access key %s last used %s days ago > %s', key, age_days, self.age)
-                print('{user:20}\t{key}\t{days:>3}\t{access_key_last_used:25}\t'\
-                      .format(user=user,
-                              key=key,
-                              days=age_days,
-                              access_key_last_used=_))
-            key += 1
+                self.print_key(user, key, active, age_days, last_used, created)
+
+    # pylint: disable=too-many-arguments
+    def print_key(self, user, key, active, age_days, last_used, created):
+        log.debug('access key %s, active: %s, last used %s days ago > %s', key, active, age_days, self.age)
+        print('{user:20}\t{key}\t{active}\t{days:>3}\t{last_used:25}\t{created}'\
+              .format(user=user,
+                      key=key,
+                      active='Active' if active else 'Inactive',
+                      days=age_days,
+                      last_used=last_used,
+                      created=created
+                     )
+             )
 
 
 if __name__ == '__main__':
