229de7ed0b9541ec0c918afbc02d98ae8dd7f1f3
Hari Sekhon <harisekhon@gmail.com>
2020-01-22 14:00:03 +0000

    updated test_getent.sh

--- tests/test_getent.sh
+++ tests/test_getent.sh
@@ -19,34 +19,38 @@ srcdir="$(cd "$(dirname "$0")" && pwd)"
 
 cd "$srcdir/.."
 
+# shellcheck disable=SC1091
 . ./bash-tools/lib/utils.sh
 
 section "Getent"
 
 start_time="$(start_timer "find_active_server.py test")"
 
-system=`uname -s`
+system="$(uname -s)"
 echo "system = $system"
 hr
 
-if [ "$system" = "Linux" -o "$system" = Darwin ]; then
+if [ "$system" = "Linux" ] ||
+   [ "$system" = Darwin ]; then
     run ./getent.py passwd | grep -v ':[x*!]*:'
     # counter is lost in subshell, increment manually
     run++
 
     # $USER isn't always available in docker containers, use 'id' instead
-    run ./getent.py passwd `id -un`
+    run ./getent.py passwd "$(id -un)"
 
     run_fail 2 ./getent.py passwd nonexistentuser
 
     run ./getent.py group | grep -v ':[x*!]:'
     run++
 
-    run ./getent.py group `id -gn`
+    run ./getent.py group "$(id -gn)"
 
     run_fail 2 ./getent.py group nonexistentgroup
 
     echo
+    # $run_count defined in lib
+    # shellcheck disable=SC2154
     echo "Tests run: $run_count"
     time_taken "$start_time" "find_active_server.py tests completed in"
 else
