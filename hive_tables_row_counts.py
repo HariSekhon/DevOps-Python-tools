f3b6279126fbc8ca014a22e9ae5eaf0c98b1e118
Hari Sekhon <harisekhon@gmail.com>
2020-01-27 17:58:43 +0000

    updated hive_tables_row_counts.py

--- hive_tables_row_counts.py
+++ hive_tables_row_counts.py
@@ -19,6 +19,8 @@
 Connect to HiveServer2 and get rows counts for all tables in all databases,
 or only those matching given db / table / partition value regexes
 
+Useful for reconciliations between clusters after migrations
+
 Tested on Hive 1.1.0 CDH 5.10 with Kerberos
 
 Due to a thrift / impyla bug this needs exactly thrift==0.9.3, see
