a79b43a99638617883d7cf59e0364e7f638a3945
Hari Sekhon <harisekhon@gmail.com>
2020-05-29 14:39:55 +0100

    updated barclaycard_crunch_accounting_csv_statement_converter.sh

--- crunch_accounting_csv_statement_converter_scripts/barclaycard_crunch_accounting_csv_statement_converter.sh
+++ crunch_accounting_csv_statement_converter_scripts/barclaycard_crunch_accounting_csv_statement_converter.sh
@@ -18,55 +18,9 @@ set -euo pipefail
 srcdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # shellcheck disable=SC1090
-#. "$srcdir/lib/utils.sh"
+. "$srcdir/lib.sh"
 
 # statements should be named in format: Barclaycard_Statement_YYYY-MM-DD.csv
-STATEMENT_GLOB="Barclaycard_Statement_[[:digit:]][[:digit:]][[:digit:]][[:digit:]]-[[:digit:]][[:digit:]]-[[:digit:]][[:digit:]].csv"
+export STATEMENT_GLOB="Barclaycard_Statement_[[:digit:]][[:digit:]][[:digit:]][[:digit:]]-[[:digit:]][[:digit:]]-[[:digit:]][[:digit:]].csv"
 
-converter="$srcdir/../crunch_accounting_csv_statement_converter.py"
-
-for statement in $STATEMENT_GLOB; do
-    crunch_statement="${statement%.csv}_crunch.csv"
-    if [ -f "$crunch_statement" ]; then
-        latest_crunch_statement="$crunch_statement"
-    fi
-done
-
-get_starting_balance(){
-    local crunch_statement="$1"
-    if ! [[ "$crunch_statement" =~ _crunch.csv$ ]]; then
-        echo "invalid statement passed to get_starting_balance, must be *_crunch.csv" >&2
-        exit 1
-    fi
-    tail -n 1 "$crunch_statement" | awk -F, '{print $4}'
-}
-
-if [ -n "${latest_crunch_statement:-}" ]; then
-    echo "latest crunch statement is $latest_crunch_statement"
-    starting_balance="$(get_starting_balance "$latest_crunch_statement")"
-else
-    starting_balance="${STARTING_BALANCE:-}"
-    if [ -n "$starting_balance" ]; then
-        echo "last crunch statement not found, you must specify the last balance manually via the environment variable \$LAST_BALANCE" >&2
-        exit 1
-    fi
-fi
-
-echo "starting balance: $starting_balance"
-
-passed_latest_statement=0
-
-for statement in $STATEMENT_GLOB; do
-    crunch_statement="${statement%.csv}_crunch.csv"
-    if [ -f "$crunch_statement" ]; then
-        if [ "$crunch_statement" = "$latest_crunch_statement" ]; then
-            passed_latest_statement=1
-        fi
-        continue
-    fi
-    if [ $passed_latest_statement -lt 1 ]; then
-        continue
-    fi
-    "$converter" --credit-card --reverse-order --starting-balance "$starting_balance" "$statement"
-    starting_balance="$(get_starting_balance "$crunch_statement")"
-done
+generate_crunch_statements
