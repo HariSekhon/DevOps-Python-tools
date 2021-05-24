6c25b875d833c6922a1ad220dced49a41ba93721
Hari Sekhon <harisekhon@gmail.com>
2021-05-24 17:02:59 +0100

    updated deploy.sh

--- gcp_cloud_function_ifconfig/deploy.sh
+++ gcp_cloud_function_ifconfig/deploy.sh
@@ -21,17 +21,22 @@ cd "$srcdir"
 
 name="ifconfig"
 
+# gcloud functions deploy doesn't seem to infer CLOUDSDK_COMPUTE_REGION from environment
+region="$(gcloud config list --format="value(compute.region)" 2>&1 || :)"
+region="${CLOUDSDK_COMPUTE_REGION:-${region:-europe-west1}}"  # not available in all regions yet
+
 # https://console.cloud.google.com/marketplace/product/google/vpcaccess.googleapis.com
 # for serverless VPC access to resources using their Private IPs
 # since we're only accessing the SQL Admin API we don't need this
-#vpc_connector="ifconfig"
+#vpc_connector="my-vpc-connector"
 
 gcloud functions deploy "$name" --trigger-http \
                                 --security-level=secure-always \
                                 --runtime python39 \
                                 --entry-point main \
                                 --memory 128MB \
+                                --region "$region" \
                                 --timeout 60 \
                                 --quiet  # don't prompt to --allow-unauthenticated
-                                #--max-instances 1
-                                #--vpc-connector "$vpc_connector"
+                                #--vpc-connector "$vpc_connector" \
+                                #--max-instances 1 \
