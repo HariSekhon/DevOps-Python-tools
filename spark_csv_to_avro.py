246d4df8036306782234ed6c1c4d47a7dee9d44e
Hari Sekhon <harisekhon@gmail.com>
2023-05-06 03:49:43 +0100

    updated spark_csv_to_avro.py

--- spark_csv_to_avro.py
+++ spark_csv_to_avro.py
@@ -57,6 +57,7 @@ os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.databricks:spark-csv_2.10:1.
                                     'com.databricks:spark-avro_2.10:2.0.1 %s' \
                                     % os.getenv('PYSPARK_SUBMIT_ARGS', '')
 pyspark_path()
+# pylint: disable=import-outside-toplevel
 from pyspark import SparkContext    # pylint: disable=wrong-import-position,import-error
 from pyspark import SparkConf       # pylint: disable=wrong-import-position,import-error
 from pyspark.sql import SQLContext  # pylint: disable=wrong-import-position,import-error
